7630bd6ca2078fb02bf2d31428d1025974ef1ade
Ross Fairgrieve <ross.fairgrieve1@gmail.com>
2020-05-09 23:28:52 +0100

    Improved region filtering and heatmap display

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 # Import data from csv
-data = pd.read_csv('data/data123.csv', header=0, index_col='siteid')
+data = pd.read_csv('data/data84.csv', header=0, index_col='siteid')
 
 # Pull out index yields and real yields into their own dataframes
 indcols = []
@@ -37,7 +37,7 @@ realyields.columns = years
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", regions = data['group'].unique())
 
 @app.route("/updategraphs", methods=["POST"])
 def updategraphs():
@@ -46,6 +46,7 @@ def updategraphs():
     bundles = request.form.get("bundles")
     targetmargin = request.form.get("targetmargin")
     minpayout = request.form.get("minpayout")
+    maxpayout = request.form.get("maxpayout")
     kgperperson = int(request.form.get("kgperperson"))
     interest = float(request.form.get("interest"))
     deposit = float(request.form.get("deposit"))
@@ -59,9 +60,10 @@ def updategraphs():
     premsaspc_list = []
 
     for strike in strikes:
+        strike = int(strike)
         # Access pre-calculated payout and premium data from the database
         # (calculation of these values is too computationally intensive to run in the app)
-        res = db.execute(f"""SELECT policyid, strike, payouts, premiums FROM policies WHERE strike={strike} AND minpayout={minpayout} AND bundles={bundles} AND targetmargin={targetmargin} ORDER BY strike""").fetchone()
+        res = db.execute(f"""SELECT policyid, strike, payouts, premiums FROM policies WHERE strike={strike} AND minpayout={minpayout} AND maxpayout={maxpayout} AND bundles={bundles} AND targetmargin={targetmargin} ORDER BY strike""").fetchone()
 
         policyid_list.append(res.policyid)
 
@@ -100,14 +102,18 @@ def updategraphs():
 @app.route("/heatmap", methods=["POST"])
 def heatmap():
     clicked_strike = request.form.get("strike")
+
     bundles = request.form.get("bundles")
     targetmargin = request.form.get("targetmargin")
     minpayout = request.form.get("minpayout")
+    maxpayout = request.form.get("maxpayout")
     kgperperson = int(request.form.get("kgperperson"))
     interest = float(request.form.get("interest"))
     deposit = float(request.form.get("deposit"))
 
-    res = db.execute(f"""SELECT strike, payouts, premiums FROM policies WHERE strike={clicked_strike} AND minpayout={minpayout} AND bundles={bundles} AND targetmargin={targetmargin} ORDER BY strike""").fetchone()
+    region = request.form.get("region")
+
+    res = db.execute(f"""SELECT strike, payouts, premiums FROM policies WHERE strike={clicked_strike} AND minpayout={minpayout} AND maxpayout={maxpayout} AND bundles={bundles} AND targetmargin={targetmargin} ORDER BY strike""").fetchone()
 
     payouts = pd.DataFrame(res.payouts).transpose()
     payouts.index = [int(x) for x in payouts.index]
@@ -131,11 +137,34 @@ def heatmap():
                                                      data['farmarea'],
                                                      startyear='1990')
 
+    min_cl = cl_noins.min().min()
+
+    cl_ins = cl_ins.sort_index(ascending=False)
+    cl_noins = cl_noins.sort_index(ascending=False)
+    indexyieldstosend = indexyields.sort_index(ascending=False)
+
+    if region != "All":
+        cl_noins = cl_noins[:][data['group'] == region]
+        cl_ins = cl_ins[:][data['group'] == region]
+        indexyieldstosend = indexyieldstosend[:][data['group'] == region]
+        regions = [region] * cl_ins.shape[0]
+    else:
+        regions = data['group'].sort_index(ascending=False).tolist()
+
+    improvement =  (cl_ins - cl_noins)
+
+    sitenames = []
+    for i in range(1, cl_ins.index.size +1):
+        sitenames.append(" " * i)
 
     heatmapdata = {"cl_noins": cl_noins.values.tolist(),
                    "cl_ins": cl_ins.values.tolist(),
-                   "improvement": (cl_ins - cl_noins).values.tolist(),
+                   "improvement": improvement.values.tolist(),
                    "columns": cl_ins.columns.tolist(),
-                   "index": cl_ins.index.tolist()}
+                   "sitenames": sitenames,
+                   "regions": regions,
+                   "min_cl": min_cl,
+                   "indexyields": indexyieldstosend.values.tolist()
+                   }
 
     return jsonify(heatmapdata)
--- static/index.js
+++ static/index.js
@@ -1,95 +1,137 @@
 
-function drawHeatmap(d) {
+
+function drawHeatmap(d=null) {
   const request = new XMLHttpRequest();
 
   const bundles = document.querySelector('#bundles').value;
   const targetmargin = document.querySelector('#targetmargin').value;
   const minpayout = document.querySelector('#minpayout').value;
+  const maxpayout = document.querySelector('#maxpayout').value;
   const kgperperson = document.querySelector('#kgperperson').value;
   const interest = document.querySelector('#interest').value;
   const deposit = document.querySelector('#deposit').value;
+  const region = document.querySelector('#region').value;
 
   var strikes = []
   for (var i = 0; i <= 6000; i += 500) {
     strikes.push(i);
   }
 
-  strike = strikes[d.points[0].pointNumber];
+  if (d == null) {
+    var strike = strikes[pointNumber]
+  } else {
+    var strike = strikes[d.points[0].pointNumber];
+  }
 
   request.open('POST', '/heatmap');
   request.onload = function() {
     heatmapData = JSON.parse(request.responseText);
 
-    // Find minimum value of cl_noins to use as heatmap scale minimum
-    var minCl = [];
-    for (var i = 0; i < heatmapData['cl_noins'].length; i++) {
-      minCl.push(Math.min.apply(null, heatmapData['cl_noins'][i]));
-    }
-    minCl = Math.min.apply(null, minCl);
+    minCl = heatmapData['min_cl']
 
     // Define and plot heatmaps
     var trace1 = {
       z: heatmapData['cl_noins'], type: 'heatmap',
       x: heatmapData['columns'],
-      y: heatmapData['index'],
+      y: [heatmapData['regions'], heatmapData['sitenames']],
+      xaxis: 'x',
+      yaxis:'y',
+      xgap: 1,
+      ygap: 1,
       zmax: 0,
       zmin: minCl,
-      colorscale: 'Hot'
+      colorscale: 'Hot',
+      showscale: false,
+      text: heatmapData['indexyields'],
+      hovertemplate:'Index-calculated yield: %{text} kg/ha'
+      // colorbar: {x: -0.18}
     };
-
-    var layout1 = {
-      showlegend: true,
-      xaxis: {visible: true},
-      yaxis: {visible: true},
-    };
-
-    var data1 = [trace1];
-
-    heatmapNo = document.getElementById('heatmap1');
-    Plotly.newPlot('heatmap1', data1, layout1);
 
     var trace2 = {
       z: heatmapData['cl_ins'], type: 'heatmap',
       x: heatmapData['columns'],
-      y: heatmapData['index'],
+      y: heatmapData['sitenames'],
+      xaxis: 'x2',
+      yaxis: 'y2',
+      xgap: 1,
+      ygap: 1,
       zmax: 0,
       zmin: minCl,
-      colorscale: 'Hot'
-      // showscale: false
+      colorscale: 'Hot',
+      showscale: false
     };
-
-    var layout2 = {
-      showlegend: false,
-      xaxis: {visible: true},
-      yaxis: {visible: true},
-    };
-
-    var data2 = [trace2];
-
-    heatmapWith = document.getElementById('heatmap2');
-    Plotly.newPlot('heatmap2', data2, layout2);
 
     var trace3 = {
       z: heatmapData['improvement'], type: 'heatmap',
       x: heatmapData['columns'],
-      y: heatmapData['index'],
+      y: heatmapData['sitenames'],
+      xaxis: 'x3',
+      yaxis: 'y3',
+      xgap: 1,
+      ygap: 1,
+      // y: heatmapData['index'],
       zmid: 0,
       zmax: 2700,
       zmin: -2700,
-      colorscale: [['0.0', 'rgb(175,0,0)'], ['0.5', 'rgb(255,255,255)'], ['1.0', 'rgb(0,175,0)']]
+      colorscale: [['0.0', 'rgb(175,0,0)'], ['0.5', 'rgb(255,255,255)'], ['1.0', 'rgb(0,175,0)']],
+      showscale: false
+      // colorbar: {x: 1}
     };
 
-    var layout3 = {
-      showlegend: true,
-      xaxis: {visible: true},
-      yaxis: {visible: true},
+    var data1 = [trace1, trace2, trace3];
+
+    var layout1 = {
+      grid: {rows: 1, columns: 3, pattern: 'independent'},
+      xaxis: {
+        showline: true,
+        linecolor: '#aaaaaa'
+        // showgrid: false
+      },
+      xaxis2: {
+        showline: true,
+        linecolor: '#aaaaaa',
+        showgrid: false
+      },
+      xaxis3: {
+        showline: true,
+        linecolor: '#aaaaaa'
+        // showgrid: false
+      },
+      yaxis: {
+        type: 'multicategory',
+        showline: true,
+        linecolor: '#cccccc',
+        tickangle: 90,
+        tickson: "boundaries",
+        ticklen: 0,
+        showdividers: true,
+        dividercolor: '#cccccc',
+        dividerwidth: 1,
+        showgrid: false,
+        automargin: true
+      },
+      yaxis2: {
+        showticklabels: false,
+        ticklen: 0,
+        showline: true,
+        linecolor: '#cccccc'
+        // showgrid: false
+      },
+      yaxis3: {
+        showticklabels: false,
+        ticklen: 0,
+        showline: true,
+        linecolor: '#cccccc'
+        // showgrid: false
+      }
+
+      // showlegend: false,
+      // xaxis: {visible: true},
+      // yaxis: {visible: true},
     };
 
-    var data3 = [trace3];
-
-    heatmapImp = document.getElementById('heatmap3');
-    Plotly.newPlot('heatmap3', data3, layout3);
-
+    heatmapCl = document.getElementById('heatmaps');
+    Plotly.newPlot('heatmaps', data1, layout1);
 
   };
 
@@ -98,10 +140,12 @@ function drawHeatmap(d) {
   hdata.append('bundles', bundles);
   hdata.append('targetmargin', targetmargin);
   hdata.append('minpayout', minpayout);
+  hdata.append('maxpayout', maxpayout);
   hdata.append('strike', strike);
   hdata.append('kgperperson', kgperperson);
   hdata.append('interest', interest);
   hdata.append('deposit', deposit);
+  hdata.append('region', region);
 
   // Send request
   request.send(hdata);
@@ -109,8 +153,13 @@ function drawHeatmap(d) {
 
 }
 
+function changePoint(e) {
+  pointNumber = e.points[0].pointNumber
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    var pointNumber = 4
     document.querySelector('#form').onsubmit = () => {
 
         // Initialize new request
@@ -119,6 +168,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const bundles = document.querySelector('#bundles').value;
         const targetmargin = document.querySelector('#targetmargin').value;
         const minpayout = document.querySelector('#minpayout').value;
+        const maxpayout = document.querySelector('#maxpayout').value;
         const kgperperson = document.querySelector('#kgperperson').value;
         const interest = document.querySelector('#interest').value;
         const deposit = document.querySelector('#deposit').value;
@@ -137,20 +187,40 @@ document.addEventListener('DOMContentLoaded', () => {
               // text: pointText,
               mode: 'markers', // could use 'lines+markers'
               marker: {
-                size: 10,
-                opacity: 0.75
-              }
+                size: 15,
+                opacity: 0.9,
+                color: '#1776a6'
+              },
+              text: graphData['strikes'],
+              hovertemplate:'<b>Strike Level:</b> %{text}kg/ha<extra></extra>',
+              hoverinfo: '',
+              line: {shape: 'spline', smoothing: 1.0}
             };
 
-            var data = [trace1];
+            var trace2 = {
+              x: graphData['premsaspc_list'],
+              y: graphData['clsr_list'],
+              // text: pointText,
+              mode: 'lines', // could use 'lines+markers'
+              marker: {
+                color: '#66aed1'
+              },
+              hoverinfo: '',
+              line: {shape: 'spline', smoothing: 1.0}
+            };
+
+            var data = [trace2, trace1];
 
             var layout = {
               xaxis: {range: [0, 50.1]},
-              yaxis: {range: [0, 100.5]}
+              yaxis: {range: [0, 100.5]},
+              hovermode: 'closest',
+              showlegend: false
             };
 
             scatterPlot = document.getElementById('result');
             Plotly.newPlot('result', data, layout);
+            scatterPlot.on('plotly_click', changePoint);
             scatterPlot.on('plotly_click', drawHeatmap);
 
 
@@ -161,6 +231,7 @@ document.addEventListener('DOMContentLoaded', () => {
         data.append('bundles', bundles);
         data.append('targetmargin', targetmargin);
         data.append('minpayout', minpayout);
+        data.append('maxpayout', maxpayout);
         data.append('kgperperson', kgperperson);
         data.append('interest', interest);
         data.append('deposit', deposit);
